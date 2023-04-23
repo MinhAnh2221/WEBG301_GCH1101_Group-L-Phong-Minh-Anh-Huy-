33d352c4317982ac796856e63f937ba5c5282956
MinhAnh2221 <131346133+MinhAnh2221@users.noreply.github.com>
2023-04-23 23:29:10 +0700

    Delete resources directory

--- resources/views/Zoo/AnimalWithRepos/sessionmessage.blade.php
+++
@@ -1,6 +0,0 @@
-@if (\Illuminate\Support\Facades\Session::get('msg') != null)
-    {{--  https://getbootstrap.com/docs/4.6/components/alerts/#dismissing--}}
-    <div style="font-size: x-large;">
-        {{\Illuminate\Support\Facades\Session::get('msg')}}
-    </div>
-@endif
